e822f7e76d26df5971a9b86dd7e1416111380e9e
duan-yf <992339621@qq.com>
2020-03-30 16:43:24 +0800

    品优购商城

---
+++ shopping/js/animate.js
@@ -0,0 +1,14 @@
+/* 动画js */
+
+function animate(obj, target, callback) {
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function() {
+        var step = (target - obj.offsetLeft) / 10;
+        step = step > 0 ? Math.ceil(step) : Math.floor(step);
+        if (obj.offsetLeft == target) {
+            clearInterval(obj.timer);
+            callback && callback();
+        }
+        obj.style.left = obj.offsetLeft + step + 'px';
+    }, 15);
+}
